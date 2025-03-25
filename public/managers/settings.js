2316957ee30b319f456a913f69fbddd1b1ac0fae
gitmotion <43588713+gitmotion@users.noreply.github.com>
2025-03-25 16:29:33 -0700

    Refactored Settings functionality into SettingsManager
    Add comment for future changes
    
    Update to fix default settings on first load

--- public/managers/settings.js
+++ public/managers/settings.js
@@ -2,18 +2,60 @@ export default class SettingsManager {
   constructor(storageManager) {
     this.storageManager = storageManager;
     this.SETTINGS_KEY = 'dumbpad_settings';
+    this.settingsInputAutoSaveStatusInterval = document.getElementById('autosave-status-interval-input');
   }
   
   getSettings() {
-    const currentSettings = this.storageManager.load(this.SETTINGS_KEY);
-    // console.log("Current Settings:", currentSettings);
-    return currentSettings;
+    try {
+      const currentSettings = this.storageManager.load(this.SETTINGS_KEY);
+      // console.log("Current Settings:", currentSettings);
+      return currentSettings;
+    } catch (err) {
+      console.error(err);
+      return false;
+    }
   }
 
-  saveSettings(settingsToSave) {
-    const currentSettings = this.getSettings();
-    const newSettings = { ...currentSettings, ...settingsToSave};
-    // console.log("Saving new settings:", newSettings);
-    this.storageManager.save(this.SETTINGS_KEY, newSettings);
+  saveSettings(appSettings, reset) {
+    try {
+      const settingsToSave = reset ? appSettings : this.getInputValues(appSettings);
+      this.storageManager.save(this.SETTINGS_KEY, settingsToSave);
+      // console.log("Saved new settings:", newSettings);
+    }
+    catch (err) {
+      console.error(err);
+    }
+  }
+
+  loadSettings(appSettings, reset) {
+    try {
+      let currentSettings = this.getSettings();
+  
+      if (reset || !currentSettings) { // Set to default settings
+        // Add default settings for additional settings below:
+        appSettings.saveStatusMessageInterval = 1000;
+  
+        currentSettings = appSettings;
+        this.saveSettings(currentSettings, true);
+      }
+  
+      // initialize/update values and inputs in app.js below:
+      appSettings.saveStatusMessageInterval = currentSettings.saveStatusMessageInterval;
+      this.settingsInputAutoSaveStatusInterval.value = currentSettings.saveStatusMessageInterval;
+      
+      return currentSettings;
+    }
+    catch (err) {
+      console.error(err);
+    }
+  }
+
+  getInputValues(appSettings) {
+    // Get and set values from inputs to appSettings
+    let newInterval = parseInt(this.settingsInputAutoSaveStatusInterval.value.trim());
+    if (isNaN(newInterval) || newInterval < 0) newInterval = null;
+    appSettings.saveStatusMessageInterval = newInterval;
+
+    return appSettings;
   }
 }
